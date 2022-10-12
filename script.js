c8144ac6fdb7ddc53a029c1866aa335aa45796a8
VItheSixth <lefoutoirdevithesixth@gmail.com>
2022-10-12 15:47:03 +0200

    Deleted php because it is not supported Added sample search bar

---
+++ script.js
@@ -0,0 +1,5 @@
+function searchResults() {
+    var searchBar = document.getElementById('searchBar');
+    document.getElementById("demo").innerHTML = "You are searching for: " + searchBar.value;
+}
+
