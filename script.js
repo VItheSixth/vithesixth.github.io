8e3e525f7ac61e71eb9746f8b552af0b6a70a449
VItheSixth <lefoutoirdevithesixth@gmail.com>
2022-10-12 16:37:50 +0200

    Testing sample json db

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+function loadMovies(){
+    fetch("movies.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => console.log(data));
+}
+
 function searchResults() {
     var searchBar = document.getElementById('searchBar');
     document.getElementById("demo").innerHTML = "You are searching for: " + searchBar.value;
 }
 
+window.addEventListener('load', loadMovies);
